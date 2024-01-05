6cd546ab9d5f9fb70653f3cab018836167faa9a2
arshidte <arshiddiyan.te.adte@gmail.com>
2024-01-05 17:57:53 +0530

    frontend started

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,6 +5,57 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
 import { protect } from "../middleware/authMiddleware.js";
 
+// Verify the user by admin and add the user to the proper position in the tree
+// after successful verification
+// BFS function to assign the user to the tree
+const bfs = async (startingUserId, newUserId) => {
+  const startingUser = await User.findById(startingUserId);
+
+  if (!startingUser) {
+    return null;
+  }
+
+  // Queue for BFS
+  const queue = [startingUser];
+
+  while (queue.length > 0) {
+    const currentNode = queue.shift();
+
+    // Determine the direction to add the new user
+    let directionToAdd = "left";
+
+    if (!currentNode.left) {
+      directionToAdd = "left";
+    } else if (!currentNode.right) {
+      directionToAdd = "right";
+    } else {
+      // Both left and right are filled, move to the next level
+      if (currentNode.left) {
+        queue.push(await User.findById(currentNode.left));
+      }
+      if (currentNode.right) {
+        queue.push(await User.findById(currentNode.right));
+      }
+      continue;
+    }
+
+    // Try to add the new user in the determined direction
+    await User.findByIdAndUpdate(currentNode._id, {
+      [directionToAdd]: newUserId,
+    });
+
+    return {
+      currentNodeId: currentNode._id,
+      directionAdded: directionToAdd,
+    };
+  }
+
+  throw new Error("Unable to assign user to the tree");
+};
+// const bfs = async ((startingUserId, newUserId) => {
+
+// });
+
 router.post(
   "/verify-user-payment",
   protect,
@@ -21,51 +72,22 @@ router.post(
 
       // Create/add user to the tree start
       // Find the last user in the binary tree to determine where to add the new user
-      const lastUser = await User.findOne({ userStatus: "approved" })
-        .sort({ createdAt: -1 })
-        .limit(1);
 
-      console.log(`The last user is ${lastUser}`);
+      // console.log(`The last user is ${lastUser}`);
 
-      if (lastUser) {
-        const sponser = await User.findById(lastUser.sponser);
+      const updateTree = await bfs(user.sponser, userId);
 
-        console.log(`sponser is ${sponser}`);
-
-        if (sponser) {
-          if (!sponser.right) {
-            const addUser = await User.findByIdAndUpdate(sponser._id, {
-              right: user._id,
-            });
-
-            if (addUser) {
-              await addUser.save();
-            }
-          }
-        } else {
-          if (!lastUser.left) {
-            const addUser = await User.findByIdAndUpdate(lastUser._id, {
-              left: user._id,
-            });
-
-            if (addUser) {
-              await addUser.save();
-            }
-          }
-        }
-      }
-
-      const updatedUser = await user.save();
-
-      if (updatedUser) {
+      
+      if (updateTree) {
         res.status(200).json({ message: "Success" });
       } else {
-        res.status(401);
-        throw new Error("Update failed. Please check again!");
+        res.status(400).json({ message: "Error assigning user to the tree" });
       }
     } else {
       res.status(401);
-      throw new Error("Can't find this user. Please check again!");
+      throw new Error(
+        "Can't find this user. Make sure you are registered properly!"
+      );
     }
   })
 );
